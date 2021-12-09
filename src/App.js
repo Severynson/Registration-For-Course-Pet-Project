a09085810270c5bd6ccbe8b3bb22ce513bad7cbf
Severyn Kurach <severynson.kurach@gmail.com>
2021-12-09 20:06:38 +0200

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      {/* <CurseDescripton /> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import classes from "./Header.module.css";
+import incubatorImg from "../../Pictures/incubator-600x450.jpg"
+
+const Header = () => {
+    return(
+        <header className={classes["main-header"]}>
+        <span className={classes["main-header-span"]}>
+          <h1>It-incubator</h1>
+          <img src={incubatorImg} className={classes["incubator-img"]} />
+        </span>
+        <span>
+          <h2>Studing platform</h2>
+        </span>
+      </header>
+    );
+};
+
+export default Header;
