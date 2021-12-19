b6a1c6e0c34753060363d7b741c8dfebf6913917
Severyn Kurach <severynson.kurach@gmail.com>
2021-12-20 00:01:17 +0200

    validationForUsernameWorks!!!Hurray!

--- src/Components/RegistrationForm/FirstForm.js
+++ src/Components/RegistrationForm/FirstForm.js
@@ -3,85 +3,47 @@ import { useState } from "react/cjs/react.development";
 import classes from "./FirstForm.module.css";
 
 const FirstForm = (props) => {
-  const [usernameWasClicked, setUsernameWasClicked] = useState(false);
-  const [passwordWasClicked, setPasswordWasClicked] = useState(false);
-  const [passwordAgainWasClicked, setPasswordAgainWasClicked] = useState(false);
-  const [usernameIsBlured, setUsernameIsBlured] = useState(false);
-  const [passwordIsBlured, setPasswordIsBlured] = useState(false);
-  const [passwordAgainIsBlured, setPasswordAgainIsBlured] = useState(false);
-  const [usernameIsVald, setUsernameIsValid] = useState(true);
-  const [passwordIsValid, setPasswordIsValid] = useState(true);
-  const [passwordAgainIsValid, setPasswordAgainIsValid] = useState(true);
-  const usernameRef = useRef("");
-  const passwordRef = useRef("");
-  const passwordAgainRef = useRef("");
-  console.log(usernameWasClicked)
-  console.log(usernameIsBlured)
+  const [username, setUsername] = useState('')
+  const [usernameWasTouched, setUsernameWasTouched] = useState(false);
+  const [usernameError, setUsernameError] = useState(false);
+
+  
+  const usernameInputHandler = (e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value);
+  };
 
   useEffect(() => {
-    if (usernameWasClicked && usernameIsBlured) {
-      setUsernameIsValid(false);
+    if (usernameWasTouched && username.trim().length < 6) {
+      setUsernameError(true);
+      console.log('Why again!!??')
+    } else {
+      setUsernameError(false);
     }
-    if (usernameWasClicked) {
-
-    }
-
-  }, [
-    usernameRef,
-    passwordRef,
-    passwordAgainRef,
-    usernameIsVald,
-    passwordIsValid,
-    passwordAgainIsValid,
-    usernameWasClicked,
-    usernameIsBlured
-  ]);
+  }, [username, usernameWasTouched])
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    props.onSubmitFirstFormHandler({
-      usernameRef: usernameRef,
-      passwordRef: passwordRef,
-      passwordAgainRef: passwordAgainRef,
-    });
+    if (username.trim() === '') return;
+    console.log("Account was created!");
+    // props.onSubmitFirstFormHandler({
+    //   usernameRef: usernameRef,
+    //   passwordRef: passwordRef,
+    //   passwordAgainRef: passwordAgainRef,
+    // });
   };
+
+
 
   return (
     <form className={classes["first-form"]} onSubmit={onSubmitHandler}>
       <label>Username:</label>
-      <input
-           onBlur={() => {
-          console.log('Username was blured')
-          setUsernameIsBlured(true);
-        }}
-        onClick={() => {
-          console.log('Username was clicked')
-          setUsernameWasClicked(true);
-        }}
-   
-        ref={usernameRef}
-      ></input>
-      {!usernameIsVald && <p>Something isn't OK in this field!</p>}
+      <input onChange={usernameInputHandler} onBlur={() => setUsernameWasTouched(true)}></input>
+      {(usernameError && <p className={classes["error-text"]}>Username is too short!</p>)}
       <label>Password:</label>
-      <input
-        onClick={() => {
-          setPasswordWasClicked(true);
-        }}
-        onBlur={() => {
-          setPasswordIsBlured(true);
-        }}
-        ref={passwordRef}
-      ></input>
+      <input ></input>
       <label>Password again:</label>
-      <input
-        onClick={() => {
-          setPasswordAgainWasClicked(true);
-        }}
-        onBlur={() => {
-          setPasswordAgainIsBlured(true);
-        }}
-        ref={passwordAgainRef}
-      ></input>
+      <input></input>
       <button type="submit">Create account</button>
     </form>
   );
